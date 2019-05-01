c2605d8acbc423ac5b5e458b9733ff24e2b606bf
陈泵 <mscb402@gmail.com>
2019-05-01 15:38:23 +0800

    博客文章更新

---
+++ js/theme.ba9945b0c16d4ddb26fd4bd58193d268e92614f0c74cced07d6d8641ac7564d7.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $(".toggler").click(function () {
+        $("nav").slideToggle();
+        $("#search").autocomplete("val", "");
+    });
+
+    $(document).keyup(function (e) {
+        if (e.keyCode === 27) {
+            if ( ! $("nav").hasClass('permanentTopNav') )
+                $("nav").slideUp();
+            $("#search").autocomplete("val", "");
+        }
+        else if (e.keyCode === 83){
+            if ( ! $("nav").hasClass('permanentTopNav') )
+                $("nav").slideDown();
+            $("#search").focus();
+        }
+    })
+
+    $('.flexslider').flexslider({
+       animation: "slide",
+       prevText: "",
+       nextText: "",
+       pauseOnHover: true,
+    });
+
+});
+
+hljs.initHighlightingOnLoad();
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?7db76b1077fe3474e7635edaaabbf236";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+
